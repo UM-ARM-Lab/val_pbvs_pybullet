a8022d3d229329a5eeb9c764796864da3759c006
hrchengmike <hrcheng@umich.edu>
2021-04-01 19:49:18 -0400

    PBVS refined, added draw_pose functionality

--- hello_world.py
+++ hello_world.py
@@ -26,7 +26,7 @@ def get_joint_states(robot):
     return joint_positions, joint_velocities, joint_torques
 
 
-def draw_cross(pos, length=0.05, width=5, color=[1.0, 0, 0]):
+def draw_cross(pos, length=0.05, width=5):
     p.addUserDebugLine(np.asarray(np.array(pos) - np.array([length, 0, 0])),
                        np.asarray(np.array(pos) + np.array([length, 0, 0])),
                        lineColorRGB=[1.0, 0, 0],
@@ -40,6 +40,21 @@ def draw_cross(pos, length=0.05, width=5, color=[1.0, 0, 0]):
                        lineColorRGB=[0, 0, 1.0],
                        lineWidth=width)
 
+
+def draw_pose(trans, rot, length=0.05, width=5, axis_len=0.1):
+    unique_ids = []
+    coords = np.array(p.getMatrixFromQuaternion(rot)).reshape(3, 3) * axis_len + np.array(trans).reshape(3, 1)
+    colors = np.eye(3)
+    for i in range(3):
+        unique_ids.append(p.addUserDebugLine(trans,
+                                             np.asarray(coords[:, i]),
+                                             lineColorRGB=np.asarray(colors[:, i]),
+                                             lineWidth=width))
+    return unique_ids
+
+def erase_pos(line_ids):
+    for line_id in line_ids:
+        p.removeUserDebugItem(line_id)
 
 def get_pos(robot, link):
     """
@@ -113,9 +128,11 @@ def get_arm_jacobian(robot, arm, tool, loc_pos=[0.0] * 3):
     else:
         print('''arm incorrect, input "left" or "right" ''')
 
+
 def quat2se3(quat):
     axis, angle = p.getAxisAngleFromQuaternion(quat)
     return np.array(axis) * angle
+
 
 class vs():
     def __init__(self):
@@ -130,6 +147,8 @@ class vs():
         physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
         p.setGravity(0, 0, -9.8)
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        p.resetDebugVisualizerCamera(cameraDistance=1.4, cameraYaw=216,
+                                     cameraPitch=-49.6, cameraTargetPosition=[0, 0, 0])
         planeId = p.loadURDF("plane.urdf")
         startPos = [0, 0, 0.2]
         startOrientation = p.getQuaternionFromEuler([0, 0, 0])
@@ -160,7 +179,7 @@ class vs():
                               omega_threshold=0.6,
                               joint_vel_threshold=1.5,
                               cond_threshold=100000,
-                              show_cond = False):
+                              show_cond=False):
         """
         Cartesian end-effector controller
         requires:
@@ -225,13 +244,14 @@ class vs():
              arm,
              goal_pos,
              goal_rot,
-             kv = 0.5,
-             kw = 0.3,
+             kv=0.9,
+             kw=0.3,
              eps_pos=0.005,
              eps_rot=0.1,
-             vs_rate = 10,
+             vs_rate=20,
              joint_vel_threshold=1.5,
-             cond_threshold=10000):
+             cond_threshold=10000,
+             plot_pose = False):
         if arm == "right":
             tool = self.right_tool
             arm_joint_indices = self.right_arm_joint_indices
@@ -244,6 +264,8 @@ class vs():
 
         while True:
             cur_pos, cur_rot = get_pose(self.robot, tool)
+            if plot_pose:
+                cur_pos_plot_id = draw_pose(cur_pos, cur_rot)
             cur_pos_inv, cur_rot_inv = p.invertTransform(cur_pos, cur_rot)
             pos_cg, rot_cg = p.multiplyTransforms(cur_pos_inv, cur_rot_inv, goal_pos, goal_rot)
             err_pos = np.linalg.norm(pos_cg)
@@ -252,14 +274,17 @@ class vs():
                 break
             else:
                 print("Error to goal, position: {:2f}, orientation: {:2f}".format(err_pos, err_rot))
-            Rsc = np.array(p.getMatrixFromQuaternion(cur_rot)).reshape(3,3)
+            Rsc = np.array(p.getMatrixFromQuaternion(cur_rot)).reshape(3, 3)
 
-            twist_local = np.hstack((np.array(pos_cg)*kv, np.array(quat2se3(rot_cg))*kw)).reshape(6,1)
-            local2global = np.block([[Rsc,              np.zeros((3,3))],
-                                     [np.zeros((3,3)),  Rsc            ]])
-            twist_global =local2global.dot(twist_local)
-            self.cartesian_vel_control(arm, np.asarray(twist_global), 1/vs_rate, show_cond = False)
+            twist_local = np.hstack((np.array(pos_cg) * kv, np.array(quat2se3(rot_cg)) * kw)).reshape(6, 1)
+            local2global = np.block([[Rsc, np.zeros((3, 3))],
+                                     [np.zeros((3, 3)), Rsc]])
+            twist_global = local2global.dot(twist_local)
+            self.cartesian_vel_control(arm, np.asarray(twist_global), 1 / vs_rate, show_cond=False)
+            if plot_pose:
+                erase_pos(cur_pos_plot_id)
         print("PBVS goal achieved!")
+
 
 val = vs()
 
@@ -317,11 +342,15 @@ for i in range(1000):
     # print(p.getJointStates(val, left_arm_joint_indices))
     time.sleep(1. / 240.)
 '''
+
 cur_pos, cur_rot = get_pose(val.robot, val.left_tool)
-goal_pos = tuple(np.asarray(np.array(cur_pos) + np.array([0.1, -0.1, -0.1])))
-goal_rot = (0, 0, 0, 1)
-draw_cross(cur_pos)
-draw_cross(goal_pos)
-val.pbvs("left", goal_pos, cur_rot)
+goal_pos = tuple(np.asarray(np.array(cur_pos) + np.array([0.05, -0.05, 0.07])))
+goal_rot = p.getQuaternionSlerp(cur_rot, (0, 0, 0, 1), 0.3)
+cur_pos_id = draw_pose(cur_pos, cur_rot)
+goal_pos_id = draw_pose(goal_pos, goal_rot)
+# erase_pos(cur_pos_id)
+# draw_cross(cur_pos)
+# draw_cross(goal_pos)
+val.pbvs("left", goal_pos, goal_rot, plot_pose=False)
 time.sleep(100)
 p.disconnect()
